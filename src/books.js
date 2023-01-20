8e63d41d359178fbc1ee6b13c7fa5d0b56867743
ttanvirr <tanvir.artisan@gmail.com>
2023-01-20 22:21:59 +0600

    first commit

---
+++ src/books.js
@@ -0,0 +1,24 @@
+import img1 from './images/book-1.jpg'
+import img2 from './images/book-2.jpg'
+import img3 from './images/book-3.jpg'
+export const books = [
+  {
+    img: img1,
+    title: 'It Starts with Us',
+    author: 'Colleen Hoover',
+    id: 1,
+  },
+
+  {
+    img: img2,
+    title: 'Atomic Habits',
+    author: 'James Clear',
+    id: 2,
+  },
+  {
+    img: img3,
+    title: 'Interesting Facts For Curious Minds',
+    author: 'Jordan Moore',
+    id: 3,
+  },
+]
